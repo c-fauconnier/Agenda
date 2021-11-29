ff742dd830551c5a48947c139d6c357378840af9
Christopher Fauconnier <christopherfauconnier@gmail.com>
2021-11-29 11:17:14 +0100

    delete method
    ajout d'une méthode de suppression

--- MVP.py
+++ MVP.py
@@ -9,11 +9,22 @@ class Agenda:
         self.data = []
 
     def add_event(self, title, day, month, year):
+        """add a dictionary in a list (self.data)"""
         new_event = {}
         new_event['title'] = title
         date = datetime.datetime(year, month, day)
         new_event['date'] = date.strftime("%x")
         self.data.append(new_event)
+
+    def delete_event(self, title):
+        """delete an item based on his name"""
+        flag = False
+        for event in self.data:
+            if title == event['title']:
+                flag = True
+                self.data.remove(event)
+        if flag == False:
+            raise ValueError("L'évenement demandé n'existe pas")
 
     @property
     def get_dates(self):
@@ -27,5 +38,6 @@ if __name__ == '__main__':
     chris = Agenda('Chris')
     chris.add_event('présentation MVP', 29, 11, 2021)
     chris.add_event('avancement du projet', 30, 11, 2021)
+    #chris.delete_event('présentation MVP')
     print(chris)
     print(chris.get_dates)
